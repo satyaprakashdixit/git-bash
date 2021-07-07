a8b388ece4e0ce873cc399b46cd935838454b520
himanshudixit <dixitsatyaprakash26@gmail.com>
2021-07-07 16:50:05 +0530

    hy

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 306, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -39,7 +39,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background("blue");
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
